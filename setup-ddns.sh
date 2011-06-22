90b63a66a6a34e89a70c32e4bf33ff11ca8154d0
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2011-06-22 18:57:11 +0900

    add setup-ddns.sh

---
+++ setup-ddns.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+if [ -z "$1" ]; then
+    echo "usage: $0 ddns-domain.example.com"
+    exit 1
+fi
+
+DOMAIN=$1
+DIR=$(pwd)
+BASENAME=$(dnssec-keygen -a HMAC-MD5 -b 512 -n HOST "$DOMAIN")
+KEY=$(awk '/Key/{print $2}' "$BASENAME.private")
+touch "$DOMAIN.key.conf"
+chmod 640 "$DOMAIN.key.conf"
+chgrp bind "$DOMAIN.key.conf" || :
+cat >"$DOMAIN.key.conf" <<EOF
+key "$DOMAIN" {
+    algorithm hmac-md5;
+    secret "$KEY";
+};
+EOF
+
+cat >"named.conf.$DOMAIN" <<EOF
+include "$DIR/$DOMAIN.key.conf";
+zone "$DOMAIN" {
+    type master;
+    file "/var/cache/bind/$DOMAIN.zone";
+    allow-query { any; };
+    //allow-transfer { secondary; };
+    //allow-update { key "$DOMAIN"; };
+    update-policy {
+        grant $DOMAIN wildcard *.$DOMAIN. A;
+        grant $DOMAIN wildcard *.$DOMAIN. AAAA;
+        grant $DOMAIN wildcard *.$DOMAIN. TXT;
+    };
+};
+EOF
+if [ ! -f "/var/cache/bind/$DOMAIN.zone" ]; then
+    cat >"$DOMAIN.zone" <<EOF
+\$TTL	1d
+@	IN    SOA    ns.example.com. root.example.com. (
+	$(date +'%Y%m%d')01	; Serial
+	1h		; Refresh
+	15m		; Retry
+	1w		; Expire
+	1h )		; Negative Cache TTL
+	IN	NS	ns.example.com.
+EOF
+fi
+
+echo "Please edit SOA and NS of $DOMAIN.zone and copy to /var/cache/bind/$DOMAIN.zone"
+echo "Please include named.conf.$DOMAIN from your named.conf"
