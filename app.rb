696a1fffd38ccc2e830d456b50c6aaac5b9305a0
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2011-06-22 23:28:59 +0900

    test auth

--- app.rb
+++ app.rb
@@ -1,9 +1,51 @@
+$LOAD_PATH.push File.expand_path('../lib', __FILE__)
+require 'ddns'
+require 'plain_auth'
+
 helpers do
   include Rack::Utils
   alias_method :h, :escape_html
+
+  def logger
+    @logger ||= Logger.new("log/app-ddns-#{ENV['RACK_ENV']}.log", 'daily')
+  end
+
+  def protected!
+    return if authorized?
+    headers 'WWW-Authenticate' => %(Basic realm="Dynamic DNS")
+    halt 401, 'Authorization Required'
+  end
+
+  def authorized?
+    @plain_auth ||= PlainAuth.new(ENV['CREDENTIALS_DIR'] || 'config/credentials')
+    @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+    @auth.provided? && @auth.basic? && @auth.credentials &&
+      @plain_auth.auth(*@auth.credentials)
+  end
+
+  def get_ddns_of(domain)
+    key, = Dir.glob("named/K#{domain}.*.key")
+    unless key
+      raise "domain not found: #{domain.inspect}"
+    end
+    DynamicDns.new(domain, key, logger)
+  end
+
+  def update_ddns(host, domain, address)
+    @ddns ||= get_ddns_of(domain)
+    @ddns.update(host, address)
+  end
 end
 
 get '/' do
   content_type "text/html", :charset => "utf-8"
   "<p>Your IP address is '#{h(request.env['REMOTE_ADDR'])}'.</p>\n"
+end
+
+get '/update' do
+  protected!
+  user = @auth.username
+  domain = @plain_auth.domain(user)
+  update_ddns(user, domain, request.env['REMOTE_ADDR'])
+  "<p>'#{h(user)}.#{h(domain)}' set to '#{h(request.env['REMOTE_ADDR'])}'.</p>\n"
 end
--- test/test_app.rb
+++ test/test_app.rb
@@ -9,6 +9,7 @@ require 'test/unit'
 require 'rack/test'
 
 ENV['RACK_ENV'] = 'test'
+ENV['CREDENTIALS_DIR'] = 'test/credentials'
 
 class AppTest < Test::Unit::TestCase
   include Rack::Test::Methods
@@ -20,6 +21,26 @@ class AppTest < Test::Unit::TestCase
   def test_get_root
     get '/'
     assert last_response.ok?
-    assert_match /\b127\.0\.0\.1\b/, last_response.body
+    assert_match(/\b127\.0\.0\.1\b/, last_response.body)
+  end
+
+  def test_get_update_without_authentication
+    get '/update'
+    assert_equal 401, last_response.status
+    assert_equal 'Authorization Required', last_response.body
+  end
+
+  def test_get_update_with_bad_credentials
+    authorize 'user', 'ng'
+    get '/update'
+    assert_equal 401, last_response.status
+    assert_equal 'Authorization Required', last_response.body
+  end
+
+  def test_get_update_with_proper_credentials
+    authorize 'user', 'ok'
+    get '/update'
+    assert_equal 200, last_response.status
+    assert_match(/\b127\.0\.0\.1\b/, last_response.body)
   end
 end
