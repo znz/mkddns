30ccb7d352cc13e3d848a840b68d62236d89d43a
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2011-06-22 19:51:45 +0900

    add test_ddns.rb

---
+++ test_ddns.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/ruby
+require 'test/unit'
+$LOAD_PATH.push File.dirname(__FILE__)
+require 'ddns'
+
+class TestDynamicDns < Test::Unit::TestCase
+  LOGGER = Logger.new(STDERR)
+  LOGGER.level = Logger::DEBUG
+
+  def test_s_new
+    ddns = DynamicDns.new('example.com', '/dev/null', LOGGER)
+    assert !!ddns
+  end
+
+  def ddns_example_com
+    key, = Dir.glob("Kddns.example.com.*.key")
+    DynamicDns.new('ddns.example.com', key, LOGGER)
+  end
+
+  def test_update_ipv4
+    ddns = ddns_example_com
+    ddns.update('test', '192.168.0.1')
+    assert_equal(['192.168.0.1'], Resolv.getaddresses('test.ddns.example.com'))
+  end
+end
