607d94bec2aeb1a61180f3a61041421728caa9ea
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2011-06-23 20:03:19 +0900

    ssh keys from config/hosts/FQDN/*.pub

--- bin/update-sshkeys.rb
+++ bin/update-sshkeys.rb
@@ -2,9 +2,14 @@
 updater = File.expand_path('../update-ddns.rb', __FILE__)
 open('.ssh/authorized_keys', 'w') do |out|
   puts "updating #{out.path}"
-  Dir.glob('config/sshpubkeys/*.pub') do |filename|
+  Dir.glob('config/hosts/*/*.pub') do |filename|
+    dir = File.dirname(filename)
+    if File.symlink?(dir)
+      puts "skip #{dir}"
+      next
+    end
+    host = File.basename(dir)
     puts "reading #{filename}"
-    host = File.basename(filename,'.pub')
     key = File.open(filename) {|f| f.read }
     key.gsub!(/^/) { %Q(command="#{updater} #{host}",no-port-forwarding,no-X11-forwarding,no-agent-forwarding,no-pty ) }
     out.puts key
